f91e0450a1fc4f3b3b7180b11fcbbf4a04342abe
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2024-07-21 17:51:12 +0900

    feat: impl tests

---
+++ pkg/ga/individual_test.go
@@ -0,0 +1,54 @@
+package ga
+
+import "testing"
+
+func TestNewGenotype(t *testing.T) {
+	cases := []struct {
+		genomeLength   int
+		expectedLength int
+	}{
+		{genomeLength: 5, expectedLength: 5},
+		{genomeLength: 10, expectedLength: 10},
+	}
+
+	for _, tc := range cases {
+		genotype := NewGenotype(tc.genomeLength)
+
+		if len(genotype.Genome) != tc.expectedLength {
+			t.Fatalf("Expected genome length %d, but got %d", tc.expectedLength, len(genotype.Genome))
+		}
+	}
+}
+
+func TestFindBestIndividual(t *testing.T) {
+	cases := []struct {
+		population      []*Individual
+		expectedFitness float64
+	}{
+		{
+			population: []*Individual{
+				{Phenotype: &Phenotype{Fitness: 1.0}},
+				{Phenotype: &Phenotype{Fitness: 2.0}},
+				{Phenotype: &Phenotype{Fitness: 3.0}},
+				{Phenotype: &Phenotype{Fitness: 0.5}},
+			},
+			expectedFitness: 3.0,
+		},
+		{
+			population: []*Individual{
+				{Phenotype: &Phenotype{Fitness: 1.0}},
+				{Phenotype: &Phenotype{Fitness: 2.0}},
+				{Phenotype: &Phenotype{Fitness: 0.5}},
+			},
+			expectedFitness: 2.0,
+		},
+	}
+
+	for _, tc := range cases {
+		best := findBestIndividual(tc.population)
+
+		if best.Phenotype.Fitness != tc.expectedFitness {
+			t.Fatalf("Expected best fitness to be %f, but got %f", tc.expectedFitness, best.Phenotype.Fitness)
+		}
+	}
+}
